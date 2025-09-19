d2db2e0e627ed279d328bb7cb0f3083116049cf2
pranavpawarr <pranavpawar503@gmail.com>
2025-09-19 23:16:51 +0530

    player body created

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock  = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    dt = 0
     print("Starting Asteroids!!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -12,7 +16,9 @@ def main():
             if event.type == pygame.QUIT:
                 return 
 
+        dt = clock.tick(60) / 1000.0
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
