4ca9693ed604f58e1c900d48e08550ec4f920f35
pranavpawarr <pranavpawar503@gmail.com>
2025-09-21 19:43:37 +0530

    shooting added with a rate limiter

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -12,18 +13,19 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    asteroids = pygame.sprite.Group()
-    Asteroid.containers = (asteroids,updatable,drawable)
-
-    AsteroidField.containers = (updatable)
-    asteroid_field = AsteroidField()
     dt = 0
-
 
     while True:
         for event in pygame.event.get():
